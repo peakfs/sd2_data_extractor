7bde4b28166e355d666ea1af79c57931657a4102
perec <peak.022@gmail.com>
2021-07-26 20:34:35 +0200

    Adds divisioncostmatrix_fields

---
+++ parser/divisioncostmatrix_fields.py
@@ -0,0 +1,31 @@
+from typing import Match
+
+from .common import Handler
+from .storage import BaseStorage
+
+
+class MatrixNameParser(Handler):
+    PATTERN = r'^.*\b(\w+)\b\sis\sMAP.*$'
+    KEY = 'cost_matrix_export_name'
+
+    def __init__(self):
+        super().__init__(self.PATTERN)
+
+    def handle(self, matches: Match, storage: BaseStorage):
+        storage.last_item = matches.group(1)
+        storage.data[matches.group(1)] = {}
+
+
+class UnitCategorySlotsParser(Handler):
+    PATTERN = r'^\s*\(\s?\b(\w+\/\w+)\b,\s*\[(.*)\]\s?\).*$'
+
+    def __init__(self):
+        super().__init__(self.PATTERN)
+
+    def handle(self, matches: Match, storage: BaseStorage):
+        costs = []
+
+        for slot_cost in matches.group(2).rstrip(',').split(','):
+            costs.append(int(slot_cost))
+
+        storage.data[storage.last_item][matches.group(1)] = costs
